580a89549ce7029b22af55c9d9df94639f39381c
kgrif113 <91581102+kgrif113@users.noreply.github.com>
2024-06-16 18:03:24 -0400

    added menu bar, embeded google maps, and added instagram

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from "./assets/logo.png";
-import IG from "./assets/IG.png"
+import IG from "./assets/IG.png";
 import "./App.css";
 
 function App() {
@@ -11,8 +11,8 @@ function App() {
           <a href="#startOfMenu">
             <h2>Dishes</h2>
           </a>
-          <a href="#location" >
-          <h2>Location</h2>
+          <a href="#location">
+            <h2>Location</h2>
           </a>
         </div>
         <h1>Menu</h1>
@@ -36,7 +36,7 @@ function App() {
           <li>Rib and Chicken | 1 Rib, 1 Leg Quarter</li>
           <li>Rib and Pork Chop | 1 Rib, 1 Pork Chop</li>
           <li>Jerk Wings | 4 Whole Wings</li>
-          <h3 id="sides" >Sides</h3>
+          <h3 id="sides">Sides</h3>
           <li>Baked Beans</li>
           <li>Corn on the Cob</li>
           <li>Pasta Salad</li>
@@ -50,11 +50,24 @@ function App() {
           <li>Half Chicken</li>
           <li>Jerk Rib Tips</li>
         </ul>
-<h1 id="location" >Location</h1>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12460.184535217519!2d-77.364549!3d38.6708078!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b657773744d461%3A0xc0d13e659f056b37!2sG%20%26%20F%20Barbeque!5e0!3m2!1sen!2sus!4v1718572516324!5m2!1sen!2sus" width="100%" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <h1 id="location">Location</h1>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12460.184535217519!2d-77.364549!3d38.6708078!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b657773744d461%3A0xc0d13e659f056b37!2sG%20%26%20F%20Barbeque!5e0!3m2!1sen!2sus!4v1718572516324!5m2!1sen!2sus"
+          width="100%"
+          height="450"
+          allowfullscreen=""
+          loading="lazy"
+          target="_blank"
+          rel="noreferrer"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
         <div id="socials">
-          <a target="_blank" id="instagram" href="https://www.instagram.com/gf_bbq/">
-          <img id="instagram" src={IG} />
+          <a
+            target="_blank"
+            id="instagram"
+            href="https://www.instagram.com/gf_bbq/"
+          >
+            <img id="instagram" src={IG} alt="Instagram logo" />
           </a>
         </div>
       </div>
