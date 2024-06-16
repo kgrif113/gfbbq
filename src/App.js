9b25db300ad96d755210fced3b0b058f50ebd430
kgrif113 <91581102+kgrif113@users.noreply.github.com>
2024-06-16 13:54:22 -0400

    netlify debugging

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import logo from "./assets/logo.png";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div id="app">
